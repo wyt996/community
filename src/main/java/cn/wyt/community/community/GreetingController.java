f41c6fbf9b92d9e327693ddc424aada7a2df4c55
wuyunting <1124350373@qq.com>
2019-09-20 21:33:37 +0800

    init repo

---
+++ src/main/java/cn/wyt/community/community/GreetingController.java
@@ -0,0 +1,32 @@
+package cn.wyt.community.community;
+/**
+ * @Author: wuyunting
+ * @Description:
+ * @Date: Create in 22:12 2019/9/19
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author     ：wuyunting
+ * @date       ：Created in 2019/9/19 22:12
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name="name",defaultValue = "wyt") String name, Model model) {
+//        name = "wyt";
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
